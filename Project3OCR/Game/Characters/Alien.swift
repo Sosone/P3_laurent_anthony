f023a2be86ca2a8e1131bd7c2916d36a36cd259e
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:50:09 +0200

    Create Alien.swift

---
+++ Project3OCR/Game/Characters/Alien.swift
@@ -0,0 +1,15 @@
+//
+//  Alien.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Alien: Character {
+
+    init() {
+        super.init (name: "Alien" , life: 60 , weapon: Knife())
+    }
+}
