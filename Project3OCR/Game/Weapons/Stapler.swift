f00337cd1a9f6d338a25c865cafa5cee8596a49b
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:50:47 +0200

    Create Stapler.swift

---
+++ Project3OCR/Game/Weapons/Stapler.swift
@@ -0,0 +1,15 @@
+//
+//  Stapler.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Stapler: Weapon {
+    
+    init() {
+      super.init(damage: 5 , name: "Stapler")
+    }
+}
