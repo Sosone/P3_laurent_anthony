998356412c09b07882c5f5a4d925218d2ed8acf7
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:51:20 +0200

    Create Weapon.swift

---
+++ Project3OCR/Weapons/Weapon.swift
@@ -0,0 +1,17 @@
+//
+//  Weapon.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Weapon {
+    
+    var damage: Int
+    
+    init(damage: Int) {
+        self.damage = damage
+    }
+}
