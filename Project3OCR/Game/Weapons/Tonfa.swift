4ebbb53c4cc6068a6dd07455795c87886c5747d1
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:50:50 +0200

    Create Tonfa.swift

---
+++ Project3OCR/Game/Weapons/Tonfa.swift
@@ -0,0 +1,15 @@
+//
+//  Tonfa.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Tonfa: Weapon {
+    
+    init() {
+        super.init(damage: 30 , name: "Tonfa")
+    }
+}
