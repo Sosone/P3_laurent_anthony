7218bbceda1adeb446ea21584011dd706a7021d5
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:50:13 +0200

    Create Fishman.swift

---
+++ Project3OCR/Game/Characters/Fishman.swift
@@ -0,0 +1,15 @@
+//
+//  Fishman.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Fishman: Character {
+
+    init() {
+        super.init (name: "Fishman" , life: 80 , weapon: Scissors())
+    }
+}
