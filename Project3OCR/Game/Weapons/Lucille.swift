3e095e87e0a8c14f7064775afd2b1d5cf3b85e1e
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:50:42 +0200

    Create Lucille.swift

---
+++ Project3OCR/Game/Weapons/Lucille.swift
@@ -0,0 +1,15 @@
+//
+//  Lucille.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Lucille: Weapon {
+
+    init() {
+        super.init(damage: 80 , name: "Lucille")
+    }
+}
