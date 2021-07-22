84e7262a864fa3443977ff946cdffa9e771f03c8
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:50:34 +0200

    Create Hammer.swift

---
+++ Project3OCR/Game/Weapons/Hammer.swift
@@ -0,0 +1,15 @@
+//
+//  Hammer.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Hammer: Weapon {
+    
+    init() {
+        super.init(damage: 40 , name: "Hammer")
+    }
+}
