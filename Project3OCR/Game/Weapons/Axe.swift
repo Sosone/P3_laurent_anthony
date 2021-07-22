0d8505dd4afa6a08d3880a1b01cd00e7edd182d3
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:50:39 +0200

    Create Axe.swift

---
+++ Project3OCR/Game/Weapons/Axe.swift
@@ -0,0 +1,15 @@
+//
+//  Axe.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Axe: Weapon {
+    
+    init() {
+      super.init(damage: 60 , name: "Axe")
+    }
+}
