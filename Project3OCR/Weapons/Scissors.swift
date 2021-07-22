7aefc8bee1febedebab75fa65635e7bfa636a0f8
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:51:12 +0200

    Create Scissors.swift

---
+++ Project3OCR/Weapons/Scissors.swift
@@ -0,0 +1,15 @@
+//
+//  Scissors.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Scissors: Weapon {
+    
+    init() {
+        super.init(damage: 15)
+    }
+}
