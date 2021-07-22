4a5be02cd8361c98999c46d1b800310475321669
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:50:16 +0200

    Create Human.swift

---
+++ Project3OCR/Game/Characters/Human.swift
@@ -0,0 +1,15 @@
+//
+//  Human.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Human: Character {
+
+    init() {
+        super.init (name: "Human" , life: 100 , weapon: FryingPan())
+    }
+}
