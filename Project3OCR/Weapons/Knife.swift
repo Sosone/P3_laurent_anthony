e69c8a0f542435949a3d58e8de71fcebd339b81f
Anthony <anthonylaurent.lr@gmail.com>
2021-07-22 10:51:07 +0200

    Create Knife.swift

---
+++ Project3OCR/Weapons/Knife.swift
@@ -0,0 +1,15 @@
+//
+//  Knife.swift
+//  jeu
+//
+//  Created by Anthony Laurent on 29/06/2021.
+//
+
+import Foundation
+
+class Knife: Weapon {
+    
+    init() {
+        super.init(damage: 20)
+    }
+}
